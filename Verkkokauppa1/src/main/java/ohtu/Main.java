e5b49bf8b6a62894b53c2080ca29aa5e362719b5
techstalgist <mikko.i.ruuskanen@gmail.com>
2017-11-06 16:48:38 +0200

    mainin muutos

--- Verkkokauppa1/src/main/java/ohtu/Main.java
+++ Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
-        Kauppa kauppa = ctx.getBean(Kauppa.class);
+        Kauppa kauppa = (Kauppa) ctx.getBean("kauppa");
 
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
@@ -27,7 +27,7 @@ public class Main {
 
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
         
-        Kirjanpito kirjanpito = ctx.getBean(Kirjanpito.class);
+        Kirjanpito kirjanpito = (Kirjanpito) ctx.getBean("kirjanpito");
         // kirjanpito
         for (String tapahtuma : kirjanpito.getTapahtumat()) {
             System.out.println(tapahtuma);
